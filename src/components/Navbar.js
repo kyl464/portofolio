67aad8ab2bab994b62e21e059946d8a3dc5dc25d
Ky <pbcit1204@gmail.com>
2025-01-13 14:18:41 +0700

    Fixing bug

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,7 +6,6 @@ const Navbar = () => {
 
   useEffect(() => {
     let lastScrollTop = 0;
-    const navbar = document.querySelector(".navbar");
 
     const handleScroll = () => {
       let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
@@ -30,7 +29,7 @@ const Navbar = () => {
   return (
     <nav className={`navbar navbar-expand-lg sticky-top shadow-sm ${isVisible ? "visible" : "hidden"}`} style={{ zIndex: 999 }}>
       <div className="container-fluid">
-        <a className="navbar-brand text-light fw-bolder ms-3" href="#">
+        <a className="navbar-brand text-light fw-bolder ms-3" href="/portfolio">
           Portofolio
         </a>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
