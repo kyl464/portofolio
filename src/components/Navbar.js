5f53cd5d221a7ba8f0470b482a2dbd40339a70d4
Ky <pbcit1204@gmail.com>
2025-01-13 14:26:42 +0700

    Fixing Bug

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -30,8 +30,6 @@ const Navbar = () => {
     <nav className={`navbar navbar-expand-lg sticky-top shadow-sm ${isVisible ? "visible" : "hidden"}`} style={{ zIndex: 999 }}>
       <div className="container-fluid">
         <a className="navbar-brand text-light fw-bolder ms-3" href="/portfolio">
-          {" "}
-          {/* Change href to a valid route */}
           Portofolio
         </a>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
